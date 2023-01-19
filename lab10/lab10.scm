2cdff75dc1747eb0c4db50a9d69419b0e9445bc3
yao chengwei <tracyyaocw@gmail.com>
2023-01-19 18:49:28 +0800

    lab10

---
+++ lab10/lab10.scm
@@ -0,0 +1,19 @@
+(define (over-or-under num1 num2) 
+    (cond
+        ((< num1 num2) -1)
+        ((= num1 num2) 0)
+        ((> num1 num2) 1)
+    )
+)
+
+(define (make-adder num) 
+
+)
+
+(define (composed f g)
+    (lambda (x) (f (g x)))
+)
+
+(define (square n) (* n n))
+
+(define (pow base exp) 'YOUR-CODE-HERE)
